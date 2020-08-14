312e4f14c3016ad6b1ae1fb6b84763dc82c2c2a4
DavidQF555 <davidfeng2003@gmail.com>
2020-08-14 09:32:38 -0700

    Created Mod Frame

---
+++ src/main/java/com/davidqf/minecraft/manicworld/ManicWorld.java
@@ -0,0 +1,21 @@
+package com.davidqf.minecraft.manicworld;
+
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@net.minecraftforge.fml.common.Mod("manicworld")
+public class ManicWorld {
+
+    public ManicWorld() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+
+}
